8ae27da23b217bccf3e8217774412abde520077f
Kenny <kskaggs@solstice-consulting.com>
2013-02-08 20:49:38 -0600

    rudimentary zooming in and out

--- CodeMap/CMMapViewController.m
+++ CodeMap/CMMapViewController.m
@@ -13,13 +13,45 @@
 #import "CMMapDisplayView.h"
 
 @interface CMMapViewController ()
+- (IBAction)inClick:(id)sender;
+- (IBAction)outClick:(id)sender;
 
 - (IBAction)mapClicked:(id)sender;
 @property (strong) IBOutlet NSScrollView *scrollView;
+@property (strong,nonatomic) CMMapDisplayView* displayView;
 
 @end
 
 @implementation CMMapViewController
+
+- (IBAction)inClick:(id)sender
+{
+    CGFloat zoomFactor = 1.3;
+    NSRect visible = [self.scrollView documentVisibleRect];
+    NSRect newrect = NSInsetRect(visible, NSWidth(visible)*(1 - 1/zoomFactor)/2.0, NSHeight(visible)*(1 - 1/zoomFactor)/2.0);
+    NSRect frame = [self.scrollView.documentView frame];
+    
+    [self.scrollView.documentView scaleUnitSquareToSize:NSMakeSize(zoomFactor, zoomFactor)];
+    [self.scrollView.documentView setFrame:NSMakeRect(0, 0, frame.size.width * zoomFactor, frame.size.height * zoomFactor)];
+    
+    [[self.scrollView documentView] scrollPoint:newrect.origin];
+    
+    [self.displayView setNeedsDisplay:YES];
+}
+
+- (IBAction)outClick:(id)sender
+{
+    CGFloat zoomFactor = 1.3;
+    NSRect visible = [self.scrollView documentVisibleRect];
+    NSRect newrect = NSOffsetRect(visible, -NSWidth(visible)*(zoomFactor - 1)/2.0, -NSHeight(visible)*(zoomFactor - 1)/2.0);
+    
+    NSRect frame = [self.scrollView.documentView frame];
+    
+    [self.scrollView.documentView scaleUnitSquareToSize:NSMakeSize(1/zoomFactor, 1/zoomFactor)];
+    [self.scrollView.documentView setFrame:NSMakeRect(0, 0, frame.size.width / zoomFactor, frame.size.height / zoomFactor)];
+    
+    [[self.scrollView documentView] scrollPoint:newrect.origin];
+}
 
 - (IBAction)mapClicked:(id)sender
 {
@@ -36,10 +68,10 @@
     }
     
     
-    CMMapDisplayView* displayView = [[CMMapDisplayView alloc] initWithFrame:CGRectMake(0, 0, 100, 100) andNodes:parser.openClass.childNodes];
-    CGRect displayFrame = displayView.frame;
+    self.displayView = [[CMMapDisplayView alloc] initWithFrame:CGRectMake(0, 0, 100, 100) andNodes:parser.openClass.childNodes];
+    CGRect displayFrame = self.displayView.frame;
     [self.scrollView.documentView setFrame:CGRectMake(0, 0, displayFrame.size.width, displayFrame.size.height)];
-    [self.scrollView.documentView addSubview:displayView];
+    [self.scrollView.documentView addSubview:self.displayView];
 }
 
 @end
