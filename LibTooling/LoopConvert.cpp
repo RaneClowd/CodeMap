1df19fa3a4db33ffa03780de5920a7cd597a7a1c
Kenny <kskaggs@solstice-consulting.com>
2014-12-15 11:20:32 -0600

    got the tool building through clang and displaying a window with GTK+ :D!

--- LibTooling/LoopConvert.cpp
+++ LibTooling/LoopConvert.cpp
@@ -6,6 +6,7 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include <gtk/gtk.h>
 
 using namespace clang::tooling;
 using namespace clang;
@@ -124,13 +125,57 @@ public:
     }
 };
 
+
+
+
+
+static void hello(GtkWidget *widget, gpointer data) {
+    g_print("Hello World\n");
+}
+
+static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer data) {
+    g_print("delete event occurred\n");
+    
+    return TRUE;
+}
+
+static void destroy(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
+}
+
+
+
+
+
 int main(int argc, const char **argv) {
     CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
     ClangTool Tool(OptionsParser.getCompilations(),
                    OptionsParser.getSourcePathList());
-    return Tool.run(newFrontendActionFactory<FindNamedClassAction>().get());
+    Tool.run(newFrontendActionFactory<FindNamedClassAction>().get());
     
-    /*if (argc > 1) {
-        runToolOnCode(new FindNamedClassAction, argv[1]);
-    }*/
+    
+    
+    char **conv = const_cast<char **>(argv);
+    gtk_init(&argc, &conv);
+    
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *button = gtk_button_new_with_label("Hello World");
+    
+    g_signal_connect(window, "delete-event", G_CALLBACK(delete_event), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
+    
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    
+    g_signal_connect(button, "clicked", G_CALLBACK(hello), NULL);
+    
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    
+    gtk_container_add(GTK_CONTAINER(window), button);
+    
+    gtk_widget_show(button);
+    gtk_widget_show(window);
+    
+    gtk_main();
+    
+    return 0;
 }
